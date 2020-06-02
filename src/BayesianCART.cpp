afba399be1d8e09467ffa37a32a1b119c648dcf3
yilunsun <yilunsun@umich.edu>
2020-06-01 23:12:02 -0400

    Revert "bug"
    This reverts commit c40b3a0b272c07d81ad092a0c102c758f868a6cd.

--- src/BayesianCART.cpp
+++ src/BayesianCART.cpp
@@ -115,7 +115,7 @@ List BayesianCART(NumericMatrix x, // baseline covariate
   //Rcout<<"2!"<<endl;
   for (int l = 0; l < nChain; l++) 
   {
-    if (verbose) Rcout<<"Chain #"<<l+1<<endl;
+    //Rcout<<"Chain #"<<l+1<<endl;
     
     NodeTree *tree = mTreeStructure.Simulate(ran, &obs, 1);
     tree->SetMinimumLeafSize(MinimumLeafSize);
@@ -133,6 +133,7 @@ List BayesianCART(NumericMatrix x, // baseline covariate
       Rcout<<"Initialization Completed."<<endl;
     }
     
+
     //
     // Initialise the proposal and MCMC classes
     //
